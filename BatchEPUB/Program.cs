e8a5787bc1ac6d03a53ce786af2b73212be8fa98
Larry Lai <awen221@gmail.com>
2022-08-20 14:32:25 +0800

    commit

---
+++ BatchEPUB/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace BatchEPUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                DirectoryInfo dir = new DirectoryInfo(arg);
+                var name = dir.Name;
+                var fullname = dir.FullName;
+                var path = dir.Parent.FullName;
+
+                var py = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "images2epub.py");
+
+                var command = $"{py} -t \"{name}\" -d rtl {fullname} {fullname}.epub";
+                Console.WriteLine(command);
+
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.Arguments = command;
+                start.StandardOutputEncoding = Encoding.UTF8;
+                start.FileName = "python";
+
+                start.CreateNoWindow = true;
+
+                start.UseShellExecute = false;
+                // 接受来自调用程序的输入信息
+                start.RedirectStandardInput = true;
+                //输出信息
+                start.RedirectStandardOutput = true;
+                // 输出错误
+                start.RedirectStandardError = true;
+
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string line = reader.ReadLine();//每次讀取一行命令列列印的資訊
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BatchEPUB/readme.txt
@@ -0,0 +1,2 @@
+need python 3.x
+Install dependencies with "pip install imagesize lxml"
